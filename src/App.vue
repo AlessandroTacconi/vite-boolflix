627aa3517a78e015056f0523b135cb0c666d8ef5
AlessandroTacconi <alessandro.tacconi.97@gmail.com>
2023-12-15 17:42:13 +0100

    feat: generazione-schede

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,61 @@
 <script>
+import axios from 'axios';
+import { store } from './store';
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
-import AppFooter from './components/AppFooter.vue';
 
 export default {
   name: 'App',
   components: {
     AppHeader,
     AppMain,
-    AppFooter,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+  methods: {
+    search() {
+      axios
+        .get(this.store.apiMovieURL, {
+          params: {
+            language: 'it',
+            api_key: this.store.MyKey,
+            query: this.store.searchMovie,
+          },
+        })
+        .then((response) => {
+          this.store.movies = response.data.results;
+          console.log(response);
+        })
+        .catch((error) => {
+          console.error('Errore nella richiesta API:', error);
+        });
+    },
+  },
+  created() {
+    this.search();
   },
 };
 </script>
 
 <template>
-  <h1>Template</h1>
+  <form @submit.prevent="search">
+    <label for="search">Search</label>
+    <input type="text" id="search" v-model="store.searchMovie" />
+    <input type="submit" />
+  </form>
+  <div>
+    <ul>
+      <li v-for="movie in store.movies">
+        <h2>{{ movie.title }}</h2>
+        <h4>{{ movie.original_title }}</h4>
+        <h4>{{ movie.original_language }}</h4>
+        <h4>{{ movie.vote_average }}</h4>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style lang="scss">
--- src/store.js
+++ src/store.js
@@ -2,6 +2,7 @@ import { reactive } from 'vue';
 
 export const store = reactive({
   movies: [],
-  apiURL: 'https://api.themoviedb.org/3/search/movie',
+  apiMovieURL: 'https://api.themoviedb.org/3/search/movie',
   MyKey: 'bd371a5f96fdbb0864d30602af8b0a46',
+  searchMovie: '',
 });
